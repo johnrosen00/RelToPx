bb6d4a069535bee88c494d7241539b8913b04cd3
John Rosen <johnrosen00@gmail.com>
2021-11-21 10:06:34 -0700

    added some helper functions

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 
 
-
-//default relative css type
 let currentRelType = "vw"
 
 let vhButton = document.getElementById("vh")
@@ -13,10 +11,8 @@ vwButton.classList.toggle("active")
 
 let r2aButton = document.getElementById("r2a")
 let a2rButton = document.getElementById("a2r")
-let currentConversionType = "r2a"
+let currentConvType = "r2a"
 
-let convType = document.getElementById("conversiontype")
-let floating = document.getElementById("floating")
 
 vwButton.addEventListener('click', ()=>{
     currentRelType = "vw"
@@ -25,3 +21,41 @@ vwButton.addEventListener('click', ()=>{
 vhButton.addEventListener('click', ()=>{
     currentRelType = "vh"
 })
+
+r2aButton.addEventListener('click', ()=>{
+    currentConvType = "r2a"
+})
+
+a2rButton.addEventListener('click', ()=>{
+    currentConvType = "a2r"
+})
+
+
+let outputbtn = document.getElementById("output")
+outputbtn.addEventListener("click", ()=> {
+    
+    document.getElementById("output").textContent = convert(currentConvType, currentRelType, parseInt(document.getElementById("amt").textContent))
+})
+
+
+function vhToPxConsole(vh) {
+    console.log( (vh/100) * window.innerHeight)
+}
+
+function vwToPxConsole(vw) {
+    console.log((vw/100) * window.innerWidth)
+}
+
+function pxToVhConsole(px) {
+    console.log(100*px/window.innerHeight)
+}
+
+function pxToVwConsole(px) {
+    console.log(100*px/window.innerWidth)
+}
+
+function convert(direction, type, amount) {
+    let rel = type == "vw" ? window.innerWidth : window.innerHeight
+
+    return direction == "a2r" ? 100*amount/rel : amount*rel/100
+}
